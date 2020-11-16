d1bf042d4ee331a09639a37113ee8141a5d1773a
Shawn-H-Wang <1295053065@qq.com>
2020-11-16 23:22:18 +0800

    Finish the command touch. Now we can create files by this command. However, we could not create files with special characters.

--- src/touch.c
+++ src/touch.c
@@ -1,8 +1,33 @@
-//
-//  touch.c
-//  t
-//
-//  Created by Shawn H Wang on 2020/11/8.
-//
-
 #include "touch.h"
+#include "Shell.h"
+
+int exec_touch(char *cmd)
+{
+    char *args = split_blank(cmd, 1);
+    if (args == NULL) 
+    {
+        printf("you don't input args");
+    }
+    else
+    {
+        while(args != NULL)
+        {
+            FILE *file;
+            file = fopen(args,"r");
+            
+            if(access(args,F_OK)==0)
+            {
+                printf("the file : \' %s \' is already exist   \n",args);
+            }
+            else{
+                if(file == NULL)
+                {
+                    file = fopen(args,"w");
+                }
+            }
+            
+            fclose(file);
+            args = split_blank(cmd, 1);
+        }
+    }
+}
--- src/touch.h
+++ src/touch.h
@@ -1,13 +1,11 @@
-//
-//  touch.h
-//  t
-//
-//  Created by Shawn H Wang on 2020/11/8.
-//
-
 #ifndef touch_h
 #define touch_h
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int exec_touch(char* cmd);
 
 #endif /* touch_h */
+
